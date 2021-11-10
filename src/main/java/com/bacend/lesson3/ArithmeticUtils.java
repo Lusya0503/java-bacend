709f6e18fb3150cd2ff2a21924870e52824f1426
Lusya0503 <lusya0513@gmail.com>
2021-11-10 16:08:43 +0700

    Удалила лишнее

--- src/main/java/com/bacend/lesson3/ArithmeticUtils.java
+++
@@ -1,9 +0,0 @@
-package com.bacend.lesson3;
-
-public class ArithmeticUtils {
-
-    public static int sum(int x, int y) {
-        return x + y;
-    }
-
-}
--- src/test/java/com/bacend/restApi/imgur/ImgurApiParams.java
+++
@@ -1,9 +0,0 @@
-package com.bacend.restApi.imgur;
-
-public class ImgurApiParams {
-
-    public static String TOKEN = "***";
-    public static String API_URL = "https://api.imgur.com";
-    public static String API_VERSION = "3";
-
-}
